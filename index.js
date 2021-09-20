003110b53088ffd5463cff89fb5d1e906782c954
liekevdvoort <49523639+liekevdvoort@users.noreply.github.com>
2021-09-20 12:01:07 +0200

    First working post

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 var express = require("express");
-
 var app = express();
 
-app.get("/getPlayers", (req, res) => {
-    res.send("Get players was called");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var content = [
+    { uploadedfile: "c:/test/test", filelocation: "Shared/testlieke" },
+];
+
+app.get("/getLocalContent", (req, res) => {
+    res.send(content);
 });
 
 app.get("/", (req, res) => {
     return res.send("Received a GET HTTP method");
 });
 
+app.post("/postContentToBsn", (req, res) => {
+    content.push(req.headers);
+    return res.send(req.headers);
+});
+
+// standards
 app.post("/", (req, res) => {
     return res.send("Received a POST HTTP method");
 });
@@ -22,6 +34,7 @@ app.delete("/", (req, res) => {
     return res.send("Received a DELETE HTTP method");
 });
 
+// listen to server
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
